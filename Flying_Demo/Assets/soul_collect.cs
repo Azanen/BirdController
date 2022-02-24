bcf6ef031e2874e3bfad033afb233c39b5d0ad5c
Cryosoul <oleduc08@gmail.com>
2022-02-24 13:58:34 -0500

    Soul prefab

--- Flying_Demo/Assets/soul_collect.cs
+++ Flying_Demo/Assets/soul_collect.cs
@@ -10,6 +10,15 @@ public class soul_collect : MonoBehaviour
    private void OnTriggerEnter(Collider other)
     {
         collectsound.Play();
+        StartCoroutine(detroy());
+    }
+
+    private IEnumerator detroy()
+    {
+        yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
+        
     }
 }
+
+
